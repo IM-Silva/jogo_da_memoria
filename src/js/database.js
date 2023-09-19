0c98ff002461f6f619d70200a0e4a67fb40ad530
Iasmin Silva <csilvaiasmin@gmail.com>
2023-09-19 16:54:17 -0300

    kenzie

---
+++ src/js/database.js
@@ -0,0 +1,47 @@
+const personagens = [
+    {
+      id: "0",
+      name: "mario",
+      img: "./src/img/Mario.png",
+    },
+    {
+      id: "1",
+      name: "luigi",
+      img: "./src/img/Luigi.png",
+    },
+    {
+      id: "2",
+      name: "yoshi",
+      img: "./src/img/Yoshi.png",
+    },
+    {
+      id: "3",
+      name: "piranha",
+      img: "./src/img/Piranha.png",
+    },
+    {
+      id: "4",
+      name: "bowser",
+      img: "./src/img/Bowser.png",
+    },
+    {
+      id: "5",
+      name: "coin",
+      img: "./src/img/Coin.png",
+    },
+    {
+      id: "6",
+      name: "dry_bones",
+      img: "./src/img/Dry_Bones.png",
+    },
+    {
+      id: "7",
+      name: "donkey_kong",
+      img: "./src/img/Donkey_kong.png",
+    },
+    {
+      id: "8",
+      name: "toad",
+      img: "./src/img/Toad.png",
+    },
+  ];
